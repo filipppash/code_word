422da601b9c6dda9862a95e2ab8d7f836f97a327
pilip.pashkevich@fmobile.by <pilip.pashkevich@fmobile.by>
2024-10-07 11:17:52 +0300

    fixed level letter logic visibility

--- src/app/view/modules/main/main.js
+++ src/app/view/modules/main/main.js
@@ -3,6 +3,7 @@ import {Core} from "../../core";
 import json1 from 'assets/mock/1.json';
 import json2 from 'assets/mock/2.json';
 import json3 from 'assets/mock/3.json';
+
 import {WinScreen} from "../../components/win-screen/win-screen";
 
 export class Main extends Core {
@@ -31,39 +32,86 @@ export class Main extends Core {
     }
 
     getMainHtml() {
+        const shortWords = this.getWordsByLength().shortWords;
+        const longWords = this.getWordsByLength().longWords;
+
         return `
-            <div class="page">
-                <div class="page-title">
-                    <span class="page-title_text">Уровень ${ this.level + 1 }</span>
-                </div>
-                <div class="page-content">
-                    ${this.getContentItemsHtml(16)} <!-- Создаем 16 пустых элементов для страницы -->
-                </div>
-                <div class="page-content_min" style="display: none;">
-                    <div class="page-content_min__item"></div>
-                </div>
-                <div class="page-ring_container">
-                    <div class="page-ring_container__item"></div>
-                </div>
+        <div class="page">
+            <div class="page-title">
+                <span class="page-title_text">Уровень ${this.level + 1}</span>
             </div>
-        `;
+            <div class="page-content">
+                ${this.getContentItemsHtml(shortWords)} 
+            </div>
+            <div class="page-content_block">
+                ${this.getLongWordsHtml(longWords)} 
+            </div>
+            <div class="page-content_min" style="display: none;">
+                <div class="page-content_min__item"></div>
+            </div>
+            <div class="page-ring_container">
+                <div class="page-ring_container__item"></div>
+            </div>
+        </div>
+    `;
     }
 
-    getContentItemsHtml(count) {
+    getLongWordsHtml(words) {
         let itemsHtml = '';
-        for (let i = 0; i < count; i++) {
-            itemsHtml += `
-                <div class="page-content_item">
-                    <span class="page-content_item__text"></span>
-                </div>
-            `;
-        }
+
+        words.forEach(word => {
+            const count = word.length; // Длина текущего слова
+
+            // Добавляем ячейки для букв слова (5 ячеек для длинных слов)
+            for (let i = 0; i < 5; i++) {
+                itemsHtml += `
+                    <div class="page-content_block__item">
+                        <span class="page-content_block__item__text"></span>
+                    </div>
+                `;
+            }
+        });
+
+        return itemsHtml;
+    }
+
+    getContentItemsHtml(words) {
+        let itemsHtml = '';
+
+        words.forEach(word => {
+            const count = word.length; // Длина текущего слова
+
+            // Добавляем ячейки для букв слова
+            for (let i = 0; i < count; i++) {
+                itemsHtml += `
+            <div class="page-content_item">
+                <span class="page-content_item__text"></span>
+            </div>
+        `;
+            }
+
+            // Добавляем пустую ячейку, если слово состоит из 3 букв
+            if (count === 3) {
+                itemsHtml += `
+            <div class="page-content_item">
+                <span class="page-content_item__text"></span>
+            </div>
+        `;
+            }
+        });
+
         return itemsHtml;
     }
 
     setupRing() {
-        const wordsForLevel = this.getWordsForLevel(this.level + 1);
-        const currentWord = wordsForLevel[this.currentWordIndex]; // Получаем текущее слово из массива
+        const shortWords = this.getWordsByLength().shortWords;
+        const longWords = this.getWordsByLength().longWords;
+
+        // Объединяем массивы в нужном порядке
+        const orderedWords = [...shortWords, ...longWords];
+
+        // Получаем текущее слово из нового массива
+        const currentWord = orderedWords[this.currentWordIndex];
         const lettersCount = currentWord.split(''); // Получаем массив букв текущего слова
         const container = document.querySelector('.page-ring_container__item');
 
@@ -72,17 +120,28 @@ export class Main extends Core {
 
         // Радиус круга для размещения букв (внутри кольца)
         const radius = 150;
-        const angleIncrement = 360 / lettersCount.length; // Угол между буквами
 
         // Создаем кнопки для каждой буквы текущего слова
         lettersCount.forEach((letter, index) => {
             const circle = document.createElement('div');
             circle.className = `page-ring_container__circle`;
 
-            // Вычисляем угол и задаем трансформацию
-            const angle = angleIncrement * index; // Угол для текущей буквы
+            // Получаем угол и радиус
+            const { angle, translateX } = this.getAngleAndTranslateX(radius, lettersCount.length, index);
+
+            // Проверяем, что угол не равен null
+            if (angle === null) return;
+
+            // Задаем трансформацию
             circle.style.position = 'absolute';
-            circle.style.transform = `rotate(${angle}deg) translate(${radius}px) rotate(-${angle}deg)`; // Позиционирование по кругу
+
+            if (lettersCount.length === 5 && index === 1) {
+                // Инвертированная трансформация для второй буквы
+                circle.style.transform = `rotate(-${angle}deg) translate(${translateX}px) rotate(${angle}deg)`;
+            } else {
+                // Обычная трансформация для остальных букв
+                circle.style.transform = `rotate(${angle}deg) translate(${translateX}px) rotate(-${angle}deg)`;
+            }
 
             // Устанавливаем размеры и другие стили
             circle.style.width = '89.94px';
@@ -94,15 +153,52 @@ export class Main extends Core {
         });
     }
 
-    getPositionClass(index, total) {
-        const angle = 360 / total; // Угол между буквами
-        const position = Math.round(index * angle);
+    getAngleAndTranslateX(radius, length, index) {
+        let angle;
+        let translateX = radius; // Изначально задаем радиус
 
-        // Определяем класс в зависимости от положения
-        if (position < 45 || position >= 315) return 'top';
-        if (position >= 45 && position < 135) return 'right';
-        if (position >= 135 && position < 225) return 'bottom';
-        return 'left';
+        if (length === 3) {
+            angle = this.getAngleForThree(index);
+        } else if (length === 4) {
+            angle = (360 / length) * index; // Для 4 букв (оставляем как есть, равномерно)
+        } else if (length === 5) {
+            angle = this.getAngleForFive(index);
+            if (index === 1) {
+                translateX = 150; // Для второй буквы устанавливаем 150
+            }
+        }
+
+        return { angle, translateX };
+    }
+
+    getAngleForThree(index) {
+        switch (index) {
+            case 0:
+                return 270;
+            case 1:
+                return 0;
+            case 2:
+                return 180;
+            default:
+                return null; // Возвращаем null, если индекс не верный
+        }
+    }
+
+    getAngleForFive(index) {
+        switch (index) {
+            case 0:
+                return 270;
+            case 1:
+                return 19; // Угол для второй буквы
+            case 2:
+                return 50;
+            case 3:
+                return 130;
+            case 4:
+                return 200;
+            default:
+                return null; // Возвращаем null, если индекс не верный
+        }
     }
 
     setupListeners() {
@@ -144,8 +240,12 @@ export class Main extends Core {
     }
 
     addLetter(letter) {
-        const wordsForLevel = this.getWordsForLevel(this.level + 1);
-        const currentWordToGuess = wordsForLevel[this.currentWordIndex];
+        const shortWords = this.getWordsByLength().shortWords;
+        const longWords = this.getWordsByLength().longWords;
+
+        // Объединяем массивы в нужном порядке
+        const orderedWords = [...shortWords, ...longWords];
+        const currentWordToGuess = orderedWords[this.currentWordIndex]; // Получаем текущее слово из нового массива
         const requiredCount = currentWordToGuess.split('').filter(l => l === letter).length;
         const currentCount = this.selectedLetters.filter(l => l === letter).length;
 
@@ -178,61 +278,36 @@ export class Main extends Core {
     }
 
     displayWord() {
-        const activeItems = document.querySelectorAll('.page-content_item');
+        const activeItems = document.querySelectorAll('.page-content_item, .page-content_block__item');
         const charArray = this.currentWord.split(''); // Разбиваем текущее слово на буквы
         let filledCount = 0; // Счетчик заполненных букв
 
         // Перебираем все ячейки
         for (let i = 0; i < activeItems.length; i++) {
             // Проверяем, что ячейка пустая и не имеет класса active
-            if (!activeItems[i].classList.contains('active') && activeItems[i].querySelector('.page-content_item__text').innerText === '') {
+            if (!activeItems[i].classList.contains('active') && activeItems[i].querySelector('.page-content_item__text, .page-content_block__item__text').innerText === '') {
                 if (filledCount < charArray.length) {
                     // Заполняем ячейку буквой
                     activeItems[i].classList.add('active'); // Добавляем класс active
-                    activeItems[i].querySelector('.page-content_item__text').innerText = charArray[filledCount]; // Заполняем буквой
+                    const textElement = activeItems[i].querySelector('.page-content_item__text, .page-content_block__item__text');
+                    if (textElement) {
+                        textElement.innerText = charArray[filledCount]; // Заполняем буквой
+                    }
                     filledCount++; // Увеличиваем счетчик
                 }
             }
         }
 
-        // Если не все буквы заполнены, добавляем новый ряд
-        if (filledCount < charArray.length) {
-            this.addNewRow(); // Добавляем новый ряд
-            this.displayWord(); // Вызываем снова, чтобы попытаться заполнить новый ряд
+        // Если слово состоит из 3-х букв, добавляем класс active для последней ячейки
+        if (charArray.length === 3 && filledCount === 3) {
+            for (let i = 0; i < activeItems.length; i++) {
+                // Находим первую незаполненную ячейку
+                if (!activeItems[i].classList.contains('active')) {
+                    activeItems[i].classList.add('active'); // Добавляем класс active
+                    break; // Выходим из цикла после добавления
+                }
+            }
         }
-    }
-
-    addNewRow() {
-        const contentContainer = document.querySelector('.page-content'); // Находим контейнер для ячеек
-        const newRowHtml = `
-        <div class="page-content_item">
-            <span class="page-content_item__text"></span>
-        </div>
-        <div class="page-content_item">
-            <span class="page-content_item__text"></span>
-        </div>
-        <div class="page-content_item">
-            <span class="page-content_item__text"></span>
-        </div>
-        <div class="page-content_item">
-            <span class="page-content_item__text"></span>
-        </div>
-    `;
-
-        // Добавляем новые ячейки в контейнер
-        contentContainer.insertAdjacentHTML('beforeend', newRowHtml);
-
-        // Получаем высоту нового элемента .page-content_item
-        const newItem = contentContainer.lastElementChild; // Последний добавленный ряд
-        const itemHeight = newItem.offsetHeight; // Высота элемента
-
-        // Добавляем отступ к блоку page-content_min
-        const minContent = document.querySelector('.page-content_min');
-        minContent.style.marginTop = (parseInt(getComputedStyle(minContent).marginTop) + itemHeight) + 'px';
-
-        // Добавляем отступ к блоку page-ring_container
-        const ringContainer = document.querySelector('.page-ring_container');
-        ringContainer.style.marginTop = (parseInt(getComputedStyle(ringContainer).marginTop) + itemHeight) + 'px';
     }
 
     resetSelection() {
@@ -273,6 +348,23 @@ export class Main extends Core {
             item.innerHTML = `<span class="page-content_min__item-text">${letter}</span>`;
             minContainer.appendChild(item); // Обновляем отображение выбранных букв
         }
+    }
+
+    getWordsByLength() {
+        const wordsForLevel = this.getWordsForLevel(this.level + 1);
+
+        const shortWords = [];
+        const longWords = [];
+
+        wordsForLevel.forEach(word => {
+            if (word.length > 4) {
+                longWords.push(word);
+            } else {
+                shortWords.push(word);
+            }
+        });
+
+        return { shortWords, longWords };
     }
 
     endGame() {
